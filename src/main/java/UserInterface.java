db74b490d25a281a26ae0ca0373239ae7ebbd06d
YamatoPotato <yamatomotaischool@gmail.com>
2026-02-12 15:09:20 -0500

    updates for class in Userinterface

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,3 +1,29 @@
 public class UserInterface{
 
+//scanf
+
+
+    public String startingInterface(){
+        //tells users what the program is for, commands like .help
+    }
+
+    public String displayList(){
+        // go through file list and print
+    }
+
+    public String errorOutput(){
+        //not valid file error
+
+        //not valid cipher error
+
+        //no files stored error
+    }
+
+    public void exit(){
+        //exit if error
+    }
+
+    public String help(){
+        //typing .help would show commands to user
+    }
 }
