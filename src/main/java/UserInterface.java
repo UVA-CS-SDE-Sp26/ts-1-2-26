83a16be96a53f68e113e30f9a1384cfb1552adff
YamatoPotato <yamatomotaischool@gmail.com>
2026-02-13 01:39:32 -0500

    updates Userinterface

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,38 +1,36 @@
-import java.util.Scanner;
-
 public class UserInterface{
-
-//scanner
-    public Scanner scan;
-
     public static void main(String[] args){
-        scan = new Scanner(System.in);
         println("Welcome to the file portal. All file are encrypted.");
-        println("Type .help to see how to use the program.");
+        println("Type -h or -help to see how to use the program.");
+        FileHandler fh = new FileHandler();
+        Cipher cph = new Cipher();
+
+        ProgramControl controller = new ProgramControl(fh,cph);
+        controller.run(args);
+
+        //code provided by William
+        String output;
+        if (args.length > 0 && (args[0].equals("-h")||args[0].equals("-help")) {
+            System.out.println(helpGuide());
+        }
+        else if (args.length == 0) {
+            output = controller.displayFileList();
+        } else if (args.length == 1) {
+            output = controller.displayFileContents(args[0], null);
+        } else {
+            output = controller.displayFileContents(args[0], args[1]);
+        }
+        System.out.println(output);
     }
 
-    public String startingInterface(){
-        //tells users what the program is for, commands like .help
+    private String helpGuide() {
+        return "--- File Portal Help ---\n" +
+                "Usage: java topsecret {file_number} {optional_key}\n" +
+                "Commands:\n" +
+                "no args: List all available files\n" +
+                "{file_number}: View contents of a specific file\n" +
+                "{file_number} [optional_key]: Decrypt and view file contents\n" +
+                "-h, -help : Show  help menu";
     }
 
-    public String displayList(){
-        // go through file list and print
-
-    }
-
-    public String errorOutput(){
-        //not valid file error
-
-        //not valid cipher error
-
-        //no files stored error
-    }
-
-    public void exit(){
-        //exit if error
-    }
-
-    public String help(){
-        //typing .help would show commands to user
-    }
 }
