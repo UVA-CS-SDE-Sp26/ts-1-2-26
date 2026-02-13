74296aed12c36070e8b826ffc0abe5b70e2658e1
YamatoPotato <yamatomotaischool@gmail.com>
2026-02-13 01:57:47 -0500

    updates Userinterface

--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -1,3 +1,17 @@
-class UserInterfaceTest{
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.List;
+class UserInterfaceTest{
+    @Test
+    public void testHelp(){
+        ProgramControl mockPC = mock(ProgramControl.class);
+        UserInterface ui = new UserInterface(mockPC); // Inject the mock
+
+        String result = ui.requestHandling(new String[]{"-h"});
+        assertTrue(result.contains("--- File Portal Help ---"));
+    }
 }
